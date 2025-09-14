b55debfa7b72a8f136ce81e8b8f12cd4ead605da
haera <25065660+haera@users.noreply.github.com>
2025-09-14 09:33:10 -0400

    Add project files.

---
+++ examples/example_x64.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "../vmtswap.hpp"
+
+std::unique_ptr<VMTSwap> g_vmtSwap;
+
+class example {
+public:
+	// RTTI example[0000] -- vtable idx 0
+	virtual void foo(int x) {
+		printf("[example] foo(x = %d)\n", x);
+	}
+};
+
+using fooFn = void(__fastcall*)(void*, int);
+void __fastcall foo_hook(void* ecx, int x)
+{
+	printf("{HOOKED} foo(x = %d)\n", x);
+
+	fooFn fooOrig = g_vmtSwap->get_orig_func<fooFn>(0);
+	fooOrig(ecx, x);
+}
+
+int main(int argc, char** argv)
+{
+	// spawn new instance of target obj
+	example* exampleInst = new example();
+
+	// hook the virtual function at index 0 (foo)
+	g_vmtSwap = std::make_unique<VMTSwap>(exampleInst);
+	g_vmtSwap->hook(0, &foo_hook);
+
+	// function call
+	exampleInst->foo(1337);
+
+	// restore original foo
+	g_vmtSwap->unhook(0);
+}
